bdde7a428bd328f40a7f932d5f8dbaaafd5b7801
Glaydson Rodrigues <glaydson012@gmail.com>
2023-04-14 14:15:33 -0300

    teste de analise

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "index";
+class teste {
+    protected $private;
+
+    public function metodo_teste(){
+        return 'metodo';
+    }
+}
+
+
+class teste_para_validacao {
+    public function metodo_teste(){
+        return 'metodo';
+    }
+}
